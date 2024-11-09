beebc2c46c40f2eafeef1a92e00533e3be4edc12
Sean Cunningham <sc.sean.cunningham@gmail.com>
2024-11-08 20:10:14 -0600

    adds shadow routes

--- TechShadow/app/queries/shadow_queries.py
+++ TechShadow/app/queries/shadow_queries.py
@@ -2,3 +2,19 @@
 
 def get_shadows():
     return "List of all shadows"
+
+
+def get_shadow(shadow_id):
+    return f"this is the shadow with ID {shadow_id}"
+
+
+def create_shadow():
+    return f"this creates a shadow"
+
+
+def update_shadow(shadow_id):
+    return f"this updates the shadow with ID {shadow_id}"
+
+
+def remove_shadow(shadow_id):
+    return f"this deletes the shadow with ID {shadow_id}"
--- TechShadow/app/routes/shadow_routes.py
+++ TechShadow/app/routes/shadow_routes.py
@@ -1,7 +1,27 @@
 from ts_app import app
-from queries.shadow_queries import get_shadows
+from queries.shadow_queries import get_shadows, get_shadow, create_shadow, update_shadow, remove_shadow
 
 
 @app.route("/shadows")
 def get_all_shadows():
     return get_shadows()
+
+
+@app.route("/shadow/<int:shadow_id>")
+def get_one_shadow(shadow_id):
+    return get_shadow(shadow_id)
+
+
+@app.route("/shadow")
+def post_shadow(shadow_id):
+    return create_shadow(shadow_id)
+
+
+@app.route("/update_shadow/<int:shadow_id>")
+def put_shadow(shadow_id):
+    return update_shadow(shadow_id)
+
+
+@app.route("/remove_shadow/<int:shadow_id>")
+def delete_shadow(shadow_id):
+    return remove_shadow(shadow_id)
