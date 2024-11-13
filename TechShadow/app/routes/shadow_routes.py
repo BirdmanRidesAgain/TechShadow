495dca7442abc2a6c9c92fe5bbf00adbbe8c208b
Sean Cunningham <sc.sean.cunningham@gmail.com>
2024-11-12 20:44:31 -0600

    update templates and static file paths

--- TechShadow/app/routes/shadow_routes.py
+++ TechShadow/app/routes/shadow_routes.py
@@ -1,4 +1,4 @@
-from flask import request
+from flask import request, render_template
 from ts_app import app
 from queries.shadow_queries import get_shadows, get_shadow, create_shadow, update_shadow, delete_shadow
 
@@ -16,7 +16,7 @@ def post_shadow():
 @app.route("/shadow/<int:shadow_id>", methods=["GET", "PUT", "DELETE"])
 def get_one_shadow(shadow_id):
     if request.method == "GET":
-        return get_shadow(shadow_id)
+        return render_template('base_template.html')
     elif request.method == "PUT":
         return update_shadow(shadow_id)
     elif request.method == "DELETE":
--- TechShadow/app/ts_app.py
+++ TechShadow/app/ts_app.py
@@ -2,8 +2,8 @@ import prefix
 
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="../templates", static_folder="../static")
 
 prefix.use_PrefixMiddleware(app)
 
-from routes import message_routes, shadow_routes, user_routes
+from routes import message_routes, shadow_routes, user_routes, template_routes
