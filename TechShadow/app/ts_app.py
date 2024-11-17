d789adf8a7464d6f687e766e77ea2668378d52eb
Sean Cunningham <sc.sean.cunningham@gmail.com>
2024-11-16 22:32:05 -0600

    Adds local db for testing purposes

--- TechShadow/app/ts_app.py
+++ TechShadow/app/ts_app.py
@@ -1,15 +1,29 @@
 import prefix
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from flask import Flask
 from dotenv import load_dotenv
+from tsdb import create_connection
 
-import os
 
 app = Flask(__name__, template_folder="../templates", static_folder="../static")
 
 
 from routes import message_routes, shadow_routes, user_routes, template_routes
 
-load_dotenv()
-test_env = os.getenv("TEST")
+@app.route("/testdb")
+def test_db():
+    conn = create_connection()
+    if conn:
+        with conn.cursor() as curr:
+            curr.execute("SELECT 'db connect success'")
+            message = curr.fetchone()
+        conn.close()
+        return message[0]
+    else:
+        return "failed to connect to db"
 
 prefix.use_PrefixMiddleware(app)
---
+++ TechShadow/tsdb.py
@@ -0,0 +1,24 @@
+import psycopg2
+import os
+
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+
+def create_connection():
+    try:
+        conn = psycopg2.connect(
+            dbname=DB_NAME,
+            user=DB_USER,
+            password=DB_PASSWORD,
+            host=DB_HOST,
+            port=DB_PORT
+        )
+        return conn
+    except Exception as e:
+        print("error connceting to db", e)
+        return None
