9d8a2983a13e74872cb76344fdb0d3f03396b846
Sean Cunningham <sc.sean.cunningham@gmail.com>
2024-11-20 23:51:58 -0600

    add config for in memory testing db

--- TechShadow/tsdb.py
+++ TechShadow/tsdb.py
@@ -1,5 +1,7 @@
 import psycopg2
 import os
+import sqlite3
+from app.queries.tables_queries import create_test_tables
 
 
 DB_USER = os.getenv("DB_USER")
@@ -12,14 +14,19 @@ DB_NAME = os.getenv("DB_NAME")
 # Connect to PostgreSQL
 def create_connection():
     try:
-        conn = psycopg2.connect(
-            dbname=DB_NAME,
-            user=DB_USER,
-            password=DB_PASSWORD,
-            host=DB_HOST,
-            port=DB_PORT
-        )
-        return conn
+        if os.getenv("TESTING") == "1":
+            conn = sqlite3.connect(":memory:")
+            create_test_tables()
+            return conn
+        else:
+            conn = psycopg2.connect(
+                dbname=DB_NAME,
+                user=DB_USER,
+                password=DB_PASSWORD,
+                host=DB_HOST,
+                port=DB_PORT
+            )
+            return conn
     except Exception as e:
         print("error connceting to db", e)
         return None
