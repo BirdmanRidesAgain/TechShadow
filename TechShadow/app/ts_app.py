4e1a44732f54df54264bd37bc9cfc96a0e196de5
Sean Cunningham <sc.sean.cunningham@gmail.com>
2024-11-20 21:09:52 -0600

    updates app run config

--- TechShadow/app/ts_app.py
+++ TechShadow/app/ts_app.py
@@ -13,4 +13,8 @@ app = Flask(__name__, template_folder="../templates", static_folder="../static")
 
 from routes import message_routes, shadow_routes, user_routes, template_routes, table_routes
 
-prefix.use_PrefixMiddleware(app)
+
+if __name__ == "__main__":
+    host = os.getenv("FLASK_RUN_HOST", "127.0.0.1")
+    port = int(os.getenv("FLASK_RUN_PORT", 5000))
+    app.run(host=host, port=port)
