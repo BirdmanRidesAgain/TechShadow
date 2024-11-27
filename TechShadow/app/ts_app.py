2956bc11a35237703ef8662865ddaf07c378d1f4
Sean Cunningham <sc.sean.cunningham@gmail.com>
2024-11-26 18:07:57 -0600

    creates testing db and complets user tests

--- TechShadow/app/ts_app.py
+++ TechShadow/app/ts_app.py
@@ -4,12 +4,19 @@ import sys
 from flask import Flask
 from dotenv import load_dotenv
 
+load_dotenv()
+
+
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-load_dotenv()
-
 app = Flask(__name__, template_folder="../templates", static_folder="../static")
+
+app.config["DB_USER"] = os.getenv("DB_USER")
+app.config["DB_PASSWORD"] = os.getenv("DB_PASSWORD")
+app.config["DB_HOST"] = os.getenv("DB_HOST")
+app.config["DB_PORT"] = os.getenv("DB_PORT")
+app.config["DB_NAME"] = os.getenv("DB_NAME")
 
 from routes.message_routes import message_bp
 from routes.shadow_routes import shadow_bp
